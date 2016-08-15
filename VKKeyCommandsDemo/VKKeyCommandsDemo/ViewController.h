186cd4552f0e8c26494f43739134da5fa9467d76
/ <65301524@qq.com>
2016-08-15 19:03:32 +0800

    commit

---
+++ VKKeyCommandsDemo/VKKeyCommandsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VKKeyCommandsDemo
+//
+//  Created by Awhisper on 16/8/12.
+//  Copyright © 2016年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VKKeyCommandsDemo/VKKeyCommandsDemo/ViewController.m
@@ -0,0 +1,42 @@
+//
+//  ViewController.m
+//  VKKeyCommandsDemo
+//
+//  Created by Awhisper on 16/8/12.
+//  Copyright © 2016年 baidu. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "VKKeyCommands.h"
+#import "VKShakeCommand.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    [[VKKeyCommands sharedInstance] registerKeyCommandWithInput:@"x" modifierFlags:UIKeyModifierCommand action:^(UIKeyCommand *command) {
+        NSLog(@"11");
+    }];
+    
+    [[VKShakeCommand sharedInstance]registerShakeCommandWithAction:^{
+        
+        NSLog(@"11");
+    }];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [[VKShakeCommand sharedInstance] removeShakeCommand];
+        
+    });
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
